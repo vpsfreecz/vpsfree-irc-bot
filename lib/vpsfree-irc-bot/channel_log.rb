4704320c2191772d8ad2b889f10edf298060e844
Jakub Skokan <jakub.skokan@havefun.cz>
2016-07-19 09:49:09 +0200

    ChannelLog: track channel users, quit handling
    When the user disconnects from the server, he is logged to leave all
    monitored channel he was in.

--- lib/vpsfree-irc-bot/channel_log.rb
+++ lib/vpsfree-irc-bot/channel_log.rb
@@ -1,3 +1,4 @@
+require 'thread'
 require 'uri'
 
 module VpsFree::Irc::Bot
@@ -21,8 +22,14 @@ module VpsFree::Irc::Bot
       arg :which, required: false
     end
 
+    def initialize(*_)
+      super
+      @users_mutex = Mutex.new
+    end
+
     def connect(m)
       @loggers = {}
+      @users = {}
     end
 
     def topic(m)
@@ -43,6 +50,14 @@ module VpsFree::Irc::Bot
 
     def join(m)
       if bot.nick == m.user.nick
+        # Remember all users connected to this channel
+        channel_users do |users|
+          m.channel.users.each_key do |u|
+            users[u.nick] ||= []
+            users[u.nick] << m.channel.to_s
+          end
+        end
+
         @loggers[m.channel.to_s] = [
             HtmlLogger.new(
                 m.channel,
@@ -57,17 +72,54 @@ module VpsFree::Irc::Bot
                 YAML_PATH,
             ),
         ]
+
+      else
+        # New user joined this channel
+        channel_users do |users|
+          users[m.user.nick] ||= []
+          users[m.user.nick] << m.channel.to_s
+        end
       end
 
       log(:join, m)
     end
 
     def leave(m, user)
-      log(:leave, m, user)
+      if m.channel.nil?
+        # The user has disconnected from the server.
+        # Log it as if he has left all monitored channels.
+        channel_users do |users|
+          if users.has_key?(user.nick)
+            users[user.nick].each do |chan|
+              @loggers[chan].each { |l| l.log(:leave, m, user) }
+            end
+
+            users.delete(user.nick)
+          end
+        end
+
+      else
+        # The user has left one concrete channel
+        channel_users { |users| users[user.nick].delete(m.channel.to_s) }
+
+        log(:leave, m, user)
+      end
     end
 
+    # Log nick change in all channels the user is in
     def nick(m)
-      log(:nick, m)
+      channel_users do |users|
+        unless users.has_key?(m.user.last_nick)
+          fail "user '#{m.user.last_nick}' not found"
+        end
+        
+        users[m.user.nick] = users[m.user.last_nick]
+        users.delete(m.user.last_nick)
+
+        users[m.user.nick].each do |chan|
+          @loggers[chan].each { |l| l.log(:nick, m) }
+        end
+      end
     end
 
     def cmd_archive(m, channel, which = nil)
@@ -103,6 +155,10 @@ module VpsFree::Irc::Bot
     protected
     def log(type, m, *args)
       @loggers[m.channel.to_s].each { |l| l.log(type, m, *args) }
+    end
+
+    def channel_users
+      @users_mutex.synchronize { yield(@users) }
     end
   end
 end
