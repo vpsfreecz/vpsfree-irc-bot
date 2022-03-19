3fb156b41d6b5af9b45ee2780c0fb7eac1ca61fe
Jakub Skokan <jakub.skokan@havefun.cz>
2022-03-19 11:56:28 +0100

    Accept multiple configs and merge them, symbolize names by default
    Keys in YAML now must not be prefixed with colon.

--- lib/vpsfree-irc-bot/cli.rb
+++ lib/vpsfree-irc-bot/cli.rb
@@ -11,9 +11,10 @@ module VpsFree::Irc::Bot
       @opts = {
         archive_dst: '.',
         api_url: 'https://api.vpsfree.cz',
+        config: [],
       }
     end
-    
+
     def run
       usage = <<END
 Usage: #{$0} [options] [-c CONFIG] | [<server> <channel...>]
@@ -27,19 +28,19 @@ END
         opts.on('-a', '--api URL', 'URL to the vpsAdmin API server') do |u|
           @opts[:api_url] = u
         end
-        
+
         opts.on('-c', '--config FILE', 'Config file') do |f|
-          @opts[:config] = f
+          @opts[:config] << f
         end
 
         opts.on('-u', '--archive-url URL', 'URL on which the web archive is available') do |u|
           @opts[:archive_url] = u
         end
-        
+
         opts.on('-d', '--archive-dir DIR', 'Where to save the web archive on disk') do |d|
           @opts[:archive_dst] = d
         end
-        
+
         opts.on('-n', '--nick NICK', "Set bot's nick (#{NAME})") do |n|
           @opts[:nick] = n
         end
@@ -47,12 +48,12 @@ END
         opts.on('-s', '--nickserv PASSWORD', 'Identify with nickserv') do |p|
           @opts[:nickserv] = p
         end
-        
+
         opts.on('-v', '--version', 'Print version and exit') do
           puts VERSION
           exit
         end
-        
+
         opts.on('-h', '--help', 'Show this help') do
           puts opts
           exit
@@ -61,43 +62,47 @@ END
 
       opt_parser.parse!
 
-      if !@opts[:config] && ARGV.size < 2
+      if @opts[:config].empty? && ARGV.size < 2
         puts opt_parser
         exit(1)
       end
 
-      if @opts[:config]
-        begin
-          cfg = YAML.load(File.read(@opts[:config]))
+      label = host = channels = nil
 
-          unless cfg.is_a?(::Hash)
-            warn "Config must yield a hash"
+      if @opts[:config].any?
+        @opts[:config].each do |cfg_path|
+          begin
+            cfg = YAML.safe_load(File.read(cfg_path), symbolize_names: true)
+
+            unless cfg.is_a?(::Hash)
+              warn "Config must yield a hash"
+              exit(1)
+            end
+
+            @opts = merge_config(@opts, cfg)
+
+          rescue Errno::ENOENT
+            warn "Config file '#{cfg_path}' does not exist"
+            exit(1)
+
+          rescue Psych::SyntaxError => e
+            warn "Invalid config file: #{e.message}"
             exit(1)
           end
 
-          @opts.update(cfg)
+          if !@opts[:server]
+            warn "Configure server"
+            exit(1)
 
-        rescue Errno::ENOENT
-          warn "Config file '#{@opts[:config]}' does not exist"
-          exit(1)
+          elsif !@opts[:channels]
+            warn "Configure channels"
+            exit(1)
+          end
 
-        rescue Psych::SyntaxError => e
-          warn "Invalid config file: #{e.message}"
-          exit(1)
+          label = @opts[:server][:label]
+          host = @opts[:server][:host]
+          channels = @opts[:channels]
         end
-
-        if !@opts[:server]
-          warn "Configure server"
-          exit(1)
-
-        elsif !@opts[:channels]
-          warn "Configure channels"
-          exit(1)
-        end
-
-        label = @opts[:server][:label]
-        host = @opts[:server][:host]
-        channels = @opts[:channels]
 
       else
         label = host = ARGV[0]
@@ -105,6 +110,17 @@ END
       end
 
       VpsFree::Irc::Bot.start(label, host, channels, @opts)
+    end
+
+    protected
+    def merge_config(src, override)
+      src.merge(override) do |k, old_v, new_v|
+        if old_v.instance_of?(Hash)
+          merge_config(old_v, new_v)
+        else
+          new_v
+        end
+      end
     end
   end
 end
